750e133448a224ddbadff98eafdd3c00447a5a1f
nicolewhite <nicole.white@neotechnology.com>
2015-05-05 15:18:37 +0100

    Add helper function for checking if a number is an integer

--- src/helper.js
+++ src/helper.js
@@ -12,5 +12,16 @@ function lcm(x, y) {
     return (x * y) / gcd(x, y);
 }
 
+function isInt(thing) {
+    if (typeof thing == "number") {
+        if (thing % 1 == 0) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
 exports.gcd = gcd;
 exports.lcm = lcm;
+exports.isInt = isInt;
